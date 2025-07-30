fc6dad480a00e7cc1b6d6100d56723e567e340b2
Nokaji <nokaji.dev@gmail.com>
2025-07-30 02:37:05 +0200

    Suppression des fichiers de configuration, des routes et des dépendances de l'API

--- routes/crew_router.py
+++
@@ -1,7 +0,0 @@
-from fastapi import APIRouter, Depends
-
-crew_router = APIRouter()
-
-@crew_router.get("/crew")
-async def get_crew():
-    return {"message": "Crew information retrieved successfully."}
